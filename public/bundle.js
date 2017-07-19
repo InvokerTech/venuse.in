31b86eb4b151f976b173a0f127f818b5bf992d88
Kapil Gorve <kapilsg186@gmail.com>
2017-07-19 13:54:41 +0530

    server configured with pretty url

--- public/bundle.js
+++ public/bundle.js
@@ -11,7 +11,7 @@ var API_URL = "https://venuse-backend.herokuapp.com/";     // eslint-disable-lin
         .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
             $locationProvider.hashPrefix('');
             // use the HTML5 History API
-          //  $locationProvider.html5Mode(true);
+            $locationProvider.html5Mode(true);
             $urlRouterProvider.otherwise('/home');
             $stateProvider
                 .state('home', {
